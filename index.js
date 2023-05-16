03d0e937d92f506ab0ae6ee3bc1beb9e58e73699
Amir <amirch357@gmail.com>
2023-05-16 11:31:28 +0500

    remove files

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const fs = require("fs")
 var bodyParser = require('body-parser');
 connectDatabase()
 app.use(cookieParser());
-// app.use(cors());
 
-app.use(express.json());
+app.use(cors())
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
